66c22dbcab6b76f7391b57574924a1e06366aa50
M Vendors <m-vendors@protonmail.com>
2021-05-23 20:44:46 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'pulpo_wms_DummyPkg',
+    __DIR__
+);
